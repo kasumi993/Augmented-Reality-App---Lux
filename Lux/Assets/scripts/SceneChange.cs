99489c1672c882c3818a3249d9e019945b0b8a9f
kasumi993 <42519251+kasumi993@users.noreply.github.com>
2021-03-16 05:02:38 +0100

    presentation screen ver1
    screen with animations and buttons (without redirection to next scene)
    left: luxi baby

---
+++ Lux/Assets/scripts/SceneChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneChange : MonoBehaviour
+{
+
+    public float startTime=0;
+
+    public void setScene(string sceneName){
+        StartCoroutine(scene(sceneName));   
+    }
+
+    public IEnumerator scene(string name){
+        yield return new WaitForSeconds(startTime);
+        SceneManager.LoadScene(name);
+    }
+
+    public void fade(GameObject objet){
+        StartCoroutine(FadeEnum(1.5f,objet));
+    }
+
+    public IEnumerator FadeEnum(float t,GameObject objet)
+    {
+        objet.GetComponent<CanvasGroup>().alpha = 1f;
+        // i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (objet.GetComponent<CanvasGroup>().alpha > 0.0f)
+        {
+            objet.GetComponent<CanvasGroup>().alpha = objet.GetComponent<CanvasGroup>().alpha - (Time.deltaTime / t);
+            // .color.a - (Time.deltaTime / t)
+            yield return null;
+        }
+    }
+
+}
---
+++ Lux/Assets/scripts/writeText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class writeText : MonoBehaviour
+{
+    public float delay=0.1f;
+    public float startTime=0;
+    Text txt;
+	string story;
+    // fullText.text = fullText.text.Replace("B", "<color=#ffffffxx>B</color>");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt = GetComponent<Text> ();
+		story = txt.text;
+		txt.text = "";
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText(){
+        yield return new WaitForSeconds(startTime);
+        foreach (char c in story) 
+		{
+			txt.text += c;
+			yield return new WaitForSeconds (0.125f);
+		}
+    }
+
+}
